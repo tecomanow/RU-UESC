f6abc3a4a69bbc59c70465fa000ff5dfee7688ef
Caio Gonçalves <caiocaio.goncalves.cic@gmail.com>
2022-07-17 20:46:00 -0300

    Implementada lógica de login

--- src/app/Components/LoginForm/index.jsx
+++ src/app/Components/LoginForm/index.jsx
@@ -1,35 +1,49 @@
 import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
 
 import { useNavigate } from 'react-router-dom';
 import { UserUseCase } from '../../../business/UseCases/UserUseCase';
-
+import { useState } from 'react';
+import { ValidationError } from 'yup';
 
 export function LoginForm() {
 
     const navigate = useNavigate();
-    
+    const [valorEmail, setValorEmail] = useState("");
+    const [valorSenha, setValorSenha] = useState("");
 
-    const userUseCase = new UserUseCase()
+
+    var handleChangeEmail = (event) => {
+      setValorEmail(event.target.value);
+    }
+    var handleChangeSenha = (event) => {
+      setValorSenha(event.target.value);
+    }
 
      var handleSubmit = async () => {
-      const response = await userUseCase.postUser("id")
+      
+      const user = {
+        email: valorEmail,
+        senha: valorSenha,
+      }
+      console.log(user)
+      //const response = await axios.post('url', user);
+      const response = true;
       if(response == true){
         navigate("/admin")
-      }else{
-        //error
       }
     }
 
     return (
-        <Form className="form-signin" onSubmit={() => navigate("/admin")}>
+        <Form className="form-signin" onSubmit={handleSubmit}>
 
             <img class="mb-4" src="https://propp.uesc.br/propp/img/logo_uesc.png" alt="" width="72" height="82"></img>
                 <h1 class="h3 mb-3 font-weight-normal">Entrar</h1>
                 <label for="inputEmail" class="sr-only">Endereço de email</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Endereço de email" required="" autofocus=""></input>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Endereço de email" required="" autofocus="" value={valorEmail} onChange={handleChangeEmail}></input>
                 <label for="inputPassword" class="sr-only">Senha</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required=""></input>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required="" value={valorSenha} onChange={handleChangeSenha}></input>
 
                 <button class="btn btn-lg btn-primary w-100" type="submit">Entrar</button>
                 <i class="mt-4 mb-3 text-muted d-block">Ciência da Computação - 2022.1</i>
