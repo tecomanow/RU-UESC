b23a58e39903ae00c1826c7e8d80d0c3637d0b01
Caio Gonçalves <caiocaio.goncalves.cic@gmail.com>
2022-07-21 01:52:25 -0300

    Refatoração de codigo

--- src/app/Components/Button/index.jsx
+++ src/app/Components/Button/index.jsx
@@ -1,5 +1,7 @@
 export function Button() {
-    return (
-        <button type="button" class="btn btn-success">Success</button>
-    );
-  }
+  return (
+    <button type="button" class="btn btn-success">
+      Success
+    </button>
+  );
+}
